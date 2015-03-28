fd8a8c25addc4bba9a0155cfed10d898a5c8adc7
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-28 14:25:34 +1100

    Made the facelist a renderable subtree

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var defaultcss = require('defaultcss');
 var fs = require('fs');
 var ObservConference = require('observ-conference');
 var gravatar = require('gravatar');
-var hg = require('mercury');
-var h = require('mercury').h;
+var h = require('virtual-hyperscript');
 
 /**
   # facelist
@@ -17,35 +16,27 @@ var h = require('mercury').h;
 
 **/
 
-function App(conference) {
-  return ObservConference(conference);
-}
-
 // declare view rendering.
-App.render = function render(state) {
-  var children = state.map(function(item) {
-    var parts = [];
-    var classes = [item.connected ? 'active' : 'inactive'];
-    var avatar = item.avatar || (item.email && gravatar.url(item.email));
-
-    if (avatar) {
-      parts.push(h('img', {
-        className: classes.join(' '),
-        title: item.id,
-        src: gravatar.url(item.email)
-      }));
-    }
-
-    return h('div.avatar', { className: item.local ? 'local' : '' }, h('div.aspect', parts));
-  });
-
-  return h('div.facelist', children);
-};
-
-module.exports = function(conference, opts) {
-  var container = (opts || {}).container || require('global/document').body;
-
+module.exports = function(opts) {
   defaultcss('facelist', fs.readFileSync(__dirname + '/default.css', 'utf8'));
-  hg.app(container, App(conference), App.render);
-};
 
+  return function(state) {
+    var children = state.peers.map(function(item) {
+      var parts = [];
+      var classes = [item.connected ? 'active' : 'inactive'];
+      var avatar = item.avatar || (item.email && gravatar.url(item.email));
+
+      if (avatar) {
+        parts.push(h('img', {
+          className: classes.join(' '),
+          title: item.id,
+          src: gravatar.url(item.email)
+        }));
+      }
+
+      return h('div.avatar', { className: item.local ? 'local' : '' }, h('div.aspect', parts));
+    });
+
+    return h('div.facelist', children);
+  };
+};
